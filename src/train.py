3918316ad85de9543574df4b9e14ad898eb65273
CubicPower <55009831+markhsia@users.noreply.github.com>
2023-09-23 11:56:01 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -8,15 +8,15 @@ from data import SampleGenerator
 gmf_config = {'alias': 'gmf_factor8neg4-implict',
               'num_epoch': 200,
               'batch_size': 1024,
-              # 'optimizer': 'sgd',
-              # 'sgd_lr': 1e-3,
-              # 'sgd_momentum': 0.9,
+              'optimizer': 'sgd',
+              'sgd_lr': 1e-3,
+              'sgd_momentum': 0.9,
               # 'optimizer': 'rmsprop',
               # 'rmsprop_lr': 1e-3,
               # 'rmsprop_alpha': 0.99,
               # 'rmsprop_momentum': 0,
-              'optimizer': 'adam',
-              'adam_lr': 1e-4,
+              #'optimizer': 'adam',
+              #'adam_lr': 1e-4,
               'num_users': 6040,
               'num_items': 3706,
               'latent_dim': 8,
@@ -25,7 +25,7 @@ gmf_config = {'alias': 'gmf_factor8neg4-implict',
               'use_cuda': True,
               'device_id': 0,
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
-
+"""
 mlp_config = {'alias': 'mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001',
               'num_epoch': 200,
               'batch_size': 256,  # 1024,
@@ -62,7 +62,7 @@ neumf_config = {'alias': 'pretrain_neumf_factor8neg4',
                 'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_Epoch100_HR0.5606_NDCG0.2463.model'),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
-
+"""
 # Load Data
 ml1m_dir = 'data/ml-1m/ratings.dat'
 ml1m_rating = pd.read_csv(ml1m_dir, sep='::', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
